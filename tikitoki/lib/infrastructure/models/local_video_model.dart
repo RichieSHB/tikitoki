c0c29cc06077896ee13071296d3b5c49d4c6214d
Ricardo <richieshb@gmail.com>
2025-03-26 17:47:51 -0600

    pantalla discover, botones y modelos

---
+++ tikitoki/lib/infrastructure/models/local_video_model.dart
@@ -0,0 +1,26 @@
+import 'package:tikitoki/domain/entities/video_post.dart';
+
+class LocalVideoModel {
+  LocalVideoModel({
+    required this.name,
+    required this.videoUrl,
+    this.likes = 0,
+    this.views = 0,
+  });
+
+  final String name;
+  final String videoUrl;
+  final int likes;
+  final int views;
+
+  factory LocalVideoModel.fromJson(Map<String, dynamic> json) =>
+      LocalVideoModel(
+        name: json['name'],
+        videoUrl: json['videoUrl'],
+        likes: json['likes'] ?? 0,
+        views: json['views'] ?? 0,
+      );
+
+  VideoPost toVideoPostEntity() =>
+      VideoPost(caption: name, videoUrl: videoUrl, likes: likes, views: views);
+}
---
+++ tikitoki/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:tikitoki/domain/entities/video_post.dart';
+import 'package:tikitoki/presentation/widgets/video_player/video_buttons.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  final List<VideoPost> videos;
+
+  const VideoScrollableView({super.key, required this.videos});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      scrollDirection: Axis.vertical,
+      physics: BouncingScrollPhysics(),
+      itemCount: videos.length,
+      itemBuilder: (BuildContext context, int index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+            Positioned(
+              right: 20,
+              bottom: 40,
+              child: VideoButtons(video: videoPost),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
